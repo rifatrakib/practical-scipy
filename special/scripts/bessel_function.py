6f4340b389af2cea648ac4dbfd72fdcc8b42d62f
rifatrakib <Rakib.1508@outlook.com>
2022-09-20 18:42:20 +0600

    deal with bessel function in script and render 3D plot

---
+++ special/scripts/bessel_function.py
@@ -0,0 +1,27 @@
+import numpy as np
+from scipy import special
+import matplotlib.pyplot as plt
+
+
+def drummed_height(n, k, distance, angle, t):
+    kth_zero = special.jn_zeros(n, k)[-1]
+    return np.cos(t) * np.cos(n * angle) * special.jn(n, distance * kth_zero)
+
+
+theta = np.r_[0:2*np.pi:50j]
+radius = np.r_[0:1:50j]
+
+x = np.array([r * np.cos(theta) for r in radius])
+y = np.array([r * np.sin(theta) for r in radius])
+z = np.array([drummed_height(1, 1, r, theta, 0.5) for r in radius])
+
+fig = plt.figure()
+
+ax = fig.add_axes(rect=(0, 0.05, 0.95, 0.95), projection="3d")
+ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap="RdBu_r", vmin=-0.5, vmax=0.5)
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_zlabel("z")
+ax.set_xticks(np.arange(-1, 1.1, 0.5))
+ax.set_yticks(np.arange(-1, 1.1, 0.5))
+plt.show()
